8a1ea987109b6586b3fd0d155a1ec7133f5918b0
etienne <etienne.jannin@gmail.com>
2025-05-08 16:27:36 -0400

    structure still

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="netscan",
+    version="1.0.0",
+    description="A simple network scanner",
+    author="Etienne",
+    author_email="etienne.jannin@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "rich",
+        "netifaces",
+    ],
+    entry_points={
+        "console_scripts": [
+            "netscan=netscan.netscan:main",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
